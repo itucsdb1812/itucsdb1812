4a7380e3442effe416ca27da9ac2f1dbacd99221
peritjan <34987142+peritjan@users.noreply.github.com>
2018-11-20 23:14:35 +0300

    Maeva in Wonderland

--- server.py
+++ server.py
@@ -12,7 +12,7 @@ musicdb.addMusic(Music("Annem","Zeki Müren","Türk Sanat Müziği","1975","Anne
 musicdb.addMusic(Music("asd","asdasd Müren","asdasd Sanat Müziği","2000","asdasd Sevgisi","asdasd","asdasd"))
 musicdb.addMusic(Music("qweqwe","Zeki qweqwe","Türk qweqwe Müziği","2005","qweqwe Sevgisi","qweqwe","qweqwe"))
 musicdb.addMusic(Music("Rolling in the Deep","Adele","Rock","2011","21","English","U.K."))
-
+musicdb.addMusic(Music("Maeva in Wonderland","Ibrahim Maalouf","Jazz","2011","Diagnostic","Instrumental","France"))
 
 
 
