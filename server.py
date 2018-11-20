9cd6792b2cf19221f6e850165909137fbbfda95b
alicank07 <alican.kurt.p@gmail.com>
2018-11-21 00:12:10 +0300

    Update server.py

--- server.py
+++ server.py
@@ -15,7 +15,7 @@ musicdb.addMusic(Music("Maeva in Wonderland","Ibrahim Maalouf","Jazz","2011","Di
 musicdb.addMusic(Music("Iron","Woodkid","Alternative","2013","Iron","English","France"))
 musicdb.addMusic(Music("Beat It","Michael Jackson","Pop","2012","Bad 25th Anniversary","English","U.S.A."))
 musicdb.addMusic(Music("The Show Must Go On","Queen","Rock","2011","2011 Remastered","English","U.K."))
-musicdb.addMusic(Music("Happy","Pharrell Williams","Pop","2013","Despicable Me 2(Original Soundtrack)","English","U.S.A."))
+musicdb.addMusic(Music("Happy","Pharrell Williams","Pop","2013","Despicable Me 2","English","U.S.A."))
 musicdb.addMusic(Music("Coesur Volant","Zaz","Jazz","2011","Single","French","French"))
 musicdb.addMusic(Music("Human","Rag'n'Bone Man","Soul","2017","Human (Deluxe)","English","U.S.A."))
 musicdb.addMusic(Music("Pump It","Black Eyed Peas","Pop","2005","Monkey Business","English","U.S.A."))
