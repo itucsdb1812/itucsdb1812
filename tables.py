38b755430fadba7138f715c1f08ddf1e53c5d96e
alicank07 <alican.kurt.p@gmail.com>
2018-11-20 23:54:08 +0300

    Create tables.py

---
+++ tables.py
@@ -0,0 +1,9 @@
+class Music:
+    def __init__(self, musicname, artist, musictype, releasedate, albumname, musiclanguage, musiccountry):
+        self.musicname = musicname
+        self.artist = artist
+        self.musictype = musictype
+        self.releasedate = releasedate
+        self.albumname = albumname
+        self.musiclanguage = musiclanguage
+        self.musiccountry = musiccountry
