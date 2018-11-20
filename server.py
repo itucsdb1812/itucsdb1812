1e71962811a2755d1ed5d0cfa981b502ff199c1f
peritjan <34987142+peritjan@users.noreply.github.com>
2018-11-20 23:19:05 +0300

    The Show Must Go On is added

--- server.py
+++ server.py
@@ -11,10 +11,12 @@ musicdb=db.music
 musicdb.addMusic(Music("Annem","Zeki Müren","Türk Sanat Müziği","1975","Anne Sevgisi","Türkçe","Türkiye"))
 musicdb.addMusic(Music("asd","asdasd Müren","asdasd Sanat Müziği","2000","asdasd Sevgisi","asdasd","asdasd"))
 musicdb.addMusic(Music("qweqwe","Zeki qweqwe","Türk qweqwe Müziği","2005","qweqwe Sevgisi","qweqwe","qweqwe"))
+musicdb.addMusic(Music("Smooth Criminal","Michael Jackson","Pop","2012","Bad 25th Anniversary","English","U.S.A."))
 musicdb.addMusic(Music("Rolling in the Deep","Adele","Rock","2011","21","English","U.K."))
 musicdb.addMusic(Music("Maeva in Wonderland","Ibrahim Maalouf","Jazz","2011","Diagnostic","Instrumental","France"))
 musicdb.addMusic(Music("Iron","Woodkid","Alternative","2013","Iron","English","France"))
 musicdb.addMusic(Music("Beat It","Michael Jackson","Pop","2012","Bad 25th Anniversary","English","U.S.A."))
+musicdb.addMusic(Music("The Show Must Go On","Queen","Rock","2011","2011 Remastered","English","U.K."))
 
 
 
