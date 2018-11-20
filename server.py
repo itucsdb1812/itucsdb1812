a1b31886b45b4bb9be1348ba5c6054d863598ac7
alicank07 <alican.kurt.p@gmail.com>
2018-11-20 21:38:20 +0300

    Modified server.py
    Add method = post for music.html

--- server.py
+++ server.py
@@ -1,29 +1,26 @@
 from flask import Flask, render_template
 from database import Database
-from tables import Music
+from tables import Book
 
 app = Flask(__name__)
 
-db=Database()
+db = Database()
+bookdb = db.book
 
-musicdb=db.music
-
-musicdb.addMusic(Music("Annem","Zeki Müren","Türk Sanat Müziği","1975","Anne Sevgisi","Türkçe","Türkiye"))
-musicdb.addMusic(Music("asd","asdasd Müren","asdasd Sanat Müziği","2000","asdasd Sevgisi","asdasd","asdasd"))
-musicdb.addMusic(Music("qweqwe","Zeki qweqwe","Türk qweqwe Müziği","2005","qweqwe Sevgisi","qweqwe","qweqwe"))
-
-
-
+bookdb.add_book(Book("Enver", 2018, "tip1", 6053326045, 784, "Türkiye İş Bankası Kültür Yayınları"))
+bookdb.add_book(Book("Hayvan Çiftliği", 2018, "tür2", 9750719387, 152, "Can Yayınları"))
+bookdb.add_book(Book("Simyacı", 2018, "tür1", 9750726439, 184, "Can Yayınları"))
+bookdb.add_book(Book("Göçüp Gidenler Koleksiyoncusu", 2018, "tür3", 6602026351, 168, "Doğan Kitap"))
+bookdb.add_book(Book("Osmanlı Gerçekleri", 2018, "tür2", 6050827644, 288, "Timaş Yayınları"))
 
 @app.route("/")
-def index():
-    return render_template("index.html")
+def home_page():
+    return render_template("home.html")
 
-@app.route("/music")
-def musics():
-    getmusics = musicdb.listAllMusic()
-    return render_template("musics.html",getmusics=getmusics)
-
+@app.route("/books")
+def books_page():
+    books = bookdb.get_books()
+    return render_template("books.html", books=sorted(books))
 
 if __name__ == "__main__":
-    app.run()
+	app.run()
