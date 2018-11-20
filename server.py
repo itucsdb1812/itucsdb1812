d31feaa85302040a40a32b28ba7bade8847ab4ab
peritjan <34987142+peritjan@users.noreply.github.com>
2018-11-20 23:12:50 +0300

    Rolling in the Deep is added

--- server.py
+++ server.py
@@ -11,6 +11,7 @@ musicdb=db.music
 musicdb.addMusic(Music("Annem","Zeki Müren","Türk Sanat Müziği","1975","Anne Sevgisi","Türkçe","Türkiye"))
 musicdb.addMusic(Music("asd","asdasd Müren","asdasd Sanat Müziği","2000","asdasd Sevgisi","asdasd","asdasd"))
 musicdb.addMusic(Music("qweqwe","Zeki qweqwe","Türk qweqwe Müziği","2005","qweqwe Sevgisi","qweqwe","qweqwe"))
+musicdb.addMusic(Music("Rolling in the Deep","Adele","Rock","2011","21","English","U.K."))
 
 
 
