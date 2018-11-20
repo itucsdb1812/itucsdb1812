a07f9c70ad291a45eff0cc5335ee1b83156ee2b0
endi <boynukaline@gmail.com>
2018-11-20 21:29:17 +0300

    changed templates folder, modified server.py, dbinit.py and added database.py, tables.py

--- server.py
+++ server.py
@@ -1,12 +1,28 @@
-from flask import Flask
-
+from flask import Flask, render_template
+from database import Database
+from tables import Music
 
 app = Flask(__name__)
 
+db=Database()
+
+musicdb=db.music
+
+musicdb.addMusic(Music("Annem","Zeki Müren","Türk Sanat Müziği","1975","Anne Sevgisi","Türkçe","Türkiye"))
+musicdb.addMusic(Music("asd","asdasd Müren","asdasd Sanat Müziği","2000","asdasd Sevgisi","asdasd","asdasd"))
+musicdb.addMusic(Music("qweqwe","Zeki qweqwe","Türk qweqwe Müziği","2005","qweqwe Sevgisi","qweqwe","qweqwe"))
+
+
+
 
 @app.route("/")
-def home_page():
-    return "Enes, Ali ve Perit!"
+def index():
+    return render_template("index.html")
+
+@app.route("/music")
+def musics():
+    getmusics = musicdb.listAllMusic()
+    return render_template("musics.html",getmusics=getmusics)
 
 
 if __name__ == "__main__":
---
+++ tables.py
@@ -0,0 +1,11 @@
+class Music:
+    def __init__(self, musicname, artist, musictype, releasedate, albumname, musiclanguage, musiccountry):
+        self.musicname = musicname
+        self.artist = artist
+        self.musictype = musictype
+        self.releasedate = releasedate
+        self.albumname = albumname
+        self.musiclanguage = musiclanguage
+        self.musiccountry = musiccountry
+
+
