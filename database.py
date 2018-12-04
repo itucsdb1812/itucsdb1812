5016ec25e25c10838098f222506ceac729b5714f
alicank07 <alican.kurt.p@gmail.com>
2018-12-04 22:26:12 +0300

    Update database.py

--- database.py
+++ database.py
@@ -16,7 +16,8 @@ class Database:
             with dbapi2.connect(self.url) as connection:
                 cursor = connection.cursor()
                 cursor.execute(
-                    "INSERT INTO MUSIC (MUSICNAME, ARTIST, MUSICTYPE, RELEASEDATE, ALBUMNAME, MUSICLANGUAGE, MUSICCOUNTRY) VALUES (%s, %s, %s, %s, %s, %s, %s)",(music.musicname, music.artist, music.musictype, music.releasedate, music.albumname, music.musiclanguage, music.musiccountry))
+                    "INSERT INTO MUSIC (MUSICNAME, ARTIST, MUSICTYPE, RELEASEDATE, ALBUMNAME, MUSICLANGUAGE, MUSICCOUNTRY) VALUES (%s, %s, %s, %s, %s, %s, %s)",
+                    (music.musicname, music.artist, music.musictype, music.releasedate, music.albumname, music.musiclanguage, music.musiccountry))
                 cursor.close()
 
 
