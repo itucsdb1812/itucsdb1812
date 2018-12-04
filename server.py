b846cb22f02e6400d0dec5c1bd5a7b9dd9c62692
alicank07 <alican.kurt.p@gmail.com>
2018-12-05 00:08:42 +0300

    Update server.py

--- server.py
+++ server.py
@@ -5,6 +5,9 @@ import psycopg2 as dbapi2
 app = Flask(__name__)
 app.secret_key = "super secret key"
 
+kekoenes = """user='aqclrrxqvuskdd' password='c6b8d1b121bfae6deb78546f2e0423cb2628f56c5cafee6c3fbfc00959622f10'
+         host='ec2-54-246-117-62.eu-west-1.compute.amazonaws.com' port=5432 dbname='d80l7qfpjcdsh0'"""
+
 # INDEX
 @app.route("/")
 def index():
@@ -13,7 +16,7 @@ def index():
 # MUSIC HTML
 @app.route("/music",methods=["POST","GET"])
 def musics():
-    connection = dbapi2.connect(url)
+    connection = dbapi2.connect(kekoenes)
     cursor = connection.cursor()
     cursor.execute("SELECT * FROM music")
     getmusics = cursor.fetchall()
