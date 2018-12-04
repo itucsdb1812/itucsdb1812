3c1200d7819f61449dbf1972b37f24ef0389a5f4
alicank07 <alican.kurt.p@gmail.com>
2018-12-04 23:53:28 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,14 +1,26 @@
-from flask import Flask, render_template
-# from database import Database
+from flask import Flask, render_template, url_for, redirect, request, session
+from wtforms import Form, StringField, TextAreaField, PasswordField, validators
+import psycopg2 as dbapi2
 
 
 app = Flask(__name__)
+app.secret_key = "super secret key"
 
-
-@app.route("/",methods=["POST","GET"])
+# INDEX
+@app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html",methods=["POST","GET"])
 
+# MUSIC HTML
+@app.route("/music",methods=["POST","GET"])
+def musics():
+    connection = dbapi2.connect(self.url)
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM music")
+    getmusics = cursor.fetchall()
+    session['getmusics'] = getmusics
+    cursor.close()
+    return render_template("musics.html")
 
 
 if __name__ == "__main__":
