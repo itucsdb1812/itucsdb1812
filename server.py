a2dce986853a0ac83794071c897bea1e4f6eaec8
alicank07 <alican.kurt.p@gmail.com>
2018-12-04 23:19:35 +0300

    Update server.py

--- server.py
+++ server.py
@@ -9,15 +9,6 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route("/music",methods=["POST","GET"])
-def musics():
-    connection = dbapi2.connect(self.url)
-    cursor = connection.cursor()
-    cursor.execute("SELECT * FROM music")
-    getmusics = cursor.fetchall()
-    session['getmusics'] = getmusics
-    cursor.close()
-    return render_template("musics.html")
 
 
 if __name__ == "__main__":
