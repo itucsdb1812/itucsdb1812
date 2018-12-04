cc2a599659fc2f0888f652c09f354fd894460f86
alicank07 <alican.kurt.p@gmail.com>
2018-12-05 00:11:16 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, url_for, redirect, request, session
+from wtforms import Form, StringField, TextAreaField, PasswordField, validators
 import psycopg2 as dbapi2
 
 
