92c19c28770b38dc3dea506ab52d4f45971aca82
alicank07 <alican.kurt.p@gmail.com>
2018-12-04 22:33:22 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,33 +1,20 @@
-from flask import Flask, render_template, url_for, redirect, request, session
+from flask import Flask, render_template
 # from database import Database
 from database import musicdb
-from tables import Music
-from wtforms import Form, StringField, TextAreaField, PasswordField, validators
-import psycopg2 as dbapi2
-
 
 app = Flask(__name__)
-app.secret_key = "super secret key"
-
 
 @app.route("/",methods=["POST","GET"])
 def index():
     return render_template("index.html")
 
-# MUSIC HTML
 @app.route("/music",methods=["POST","GET"])
 def musics():
-    connection = dbapi2.connect(self.url)
-    cursor = connection.cursor()
-    cursor.execute("SELECT * FROM music")
-    getmusics = cursor.fetchall()
-    session['getmusics'] = getmusics
-    cursor.close()
-    return render_template("musics.html")
-
+    getmusics = musicdb.listAllMusic()
+    return render_template("musics.html", getmusics=getmusics)
 
 @app.route("/addmusic",methods=["POST","GET"])
-def addmusictolist():
+def addmusictolist:
     return render_template("index.html")
 
 
